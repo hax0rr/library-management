2e71cab2dea7e787da0ac175d1cd6a26096b490d
Neel Vikmani <neel.vikmani@gmail.com>
2018-10-10 01:56:15 +0530

    Login and add books functionality

---
+++ LibraryApplication/add_books.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LibraryApplication
+{
+	public partial class add_books : Form
+	{
+		SqlConnection conn = new SqlConnection(@"Data Source=HAX0RR\SQLEXPRESS;Initial Catalog=library_management;Integrated Security=True;Pooling=False");
+		public add_books()
+		{
+			InitializeComponent();
+		}
+
+		private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			conn.Open();
+			SqlCommand cmd = conn.CreateCommand();
+			cmd.CommandType = CommandType.Text;
+			cmd.CommandText = "insert into books_info values('"+textBox1.Text +"','"+ textBox2.Text + "','"+ textBox3.Text  + "','"+ dateTimePicker1.Text + "',"+ textBox5.Text  + ","+ textBox6.Text  + ")";
+			cmd.ExecuteNonQuery();
+			conn.Close();
+			textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = ""; textBox5.Text = ""; textBox6.Text = "";
+
+			MessageBox.Show("Books Added!!");
+		}
+	}
+}
---
+++ LibraryApplication/login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace LibraryApplication
+{
+    public partial class login : Form
+    {
+		int count = 0;
+		SqlConnection conn = new SqlConnection(@"Data Source=HAX0RR\SQLEXPRESS;Initial Catalog=library_management;Integrated Security=True;Pooling=False");
+        public login()
+        {
+            InitializeComponent();
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SqlCommand cmd = conn.CreateCommand();
+			cmd.CommandType = CommandType.Text;
+			cmd.CommandText = "select * from librarian where username='"+textBox1.Text +"' and password='"+textBox2.Text +"'";
+			cmd.ExecuteNonQuery();
+			DataTable dt = new DataTable();
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			da.Fill(dt);
+			count = Convert.ToInt32(dt.Rows.Count.ToString());
+			if (count == 0)
+			{
+				MessageBox.Show("Username and password does not match!!");
+			}
+			else
+			{
+				this.Hide();
+				mdi_user mu = new mdi_user();
+				mu.Show();
+			}
+		}
+
+		private void login_Load(object sender, EventArgs e)
+		{
+			if(conn.State == ConnectionState.Open)
+			{
+				conn.Close();
+			}
+			conn.Open();
+		}
+	}
+}
